666da5bfa063ff759ebe240814dda71a42a8b9ad
arjunnoob <64577966+arjunnoob@users.noreply.github.com>
2021-06-21 20:15:55 +0530

    Update BalanceParenthesis.java

--- BalanceParenthesis.java
+++ BalanceParenthesis.java
@@ -3,11 +3,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-
-/**
- * @author Aakansha Doshi
- *
- */
 public class BalanceParenthesis {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
